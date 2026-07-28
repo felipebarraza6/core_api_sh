2fdd2a3766cc275ec5c8dd14dbbe72cb09589e31
Equipo Desarrollo Core API <desarrollo@coreapi.com>
2026-07-28 22:18:55 +0000

    chore: limpiar archivo temporal check_twin.py

--- tmp/check_twin.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-import django
-django.setup()
-from api.core.models import CatchmentPoint
-point = CatchmentPoint.objects.get(id=1)
-print("Point:", point.title)
-print("Is TWIN:", getattr(point, 'is_twin', 'N/A'))
-print("Frecuency:", point.frecuency)
